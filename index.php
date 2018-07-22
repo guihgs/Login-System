084a4970e86bf5e76eab8092f1a846f71081387e
Your Name <guihqueiroz.15@gmail.com>
2018-07-22 20:56:50 -0300

    Login System PHP

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
-<?php
+<?php include "header.php"; ?>
 
-echo "Hello World";
+  	<div class="uk-section uk-container">
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
 
-?>
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+			</form>
+  		</div>
+  	</div>
+
+
+<?php include "footer.php"; ?>
